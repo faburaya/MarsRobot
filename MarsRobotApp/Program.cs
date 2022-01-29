224ebc6a78b204c40b48486c5ee81019d67a930d
Felipe Aburaya <felipe.aburaya@hotmail.com>
2022-01-29 12:34:07 -0300

    Implementiert eine grundlegende Anwendung.

--- MarsRobotApp/Program.cs
+++ MarsRobotApp/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MarsRobot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
-            Console.WriteLine("Hello World!");
+            IList<int> dimension = Console.ReadLine()
+                .Split('x').Select(s => int.Parse(s)).ToList();
+
+            Robot robot = new(dimension[0], dimension[1]);
+            string commands = Console.ReadLine();
+            CommandParser.ParseAndExecute(commands, robot);
+            Console.WriteLine($"{robot.Position.x},{robot.Position.y},{robot.Direction}");
         }
     }
 }
