e156ec893d210239229d00473760cbee6a7acab4
Felipe Aburaya <felipe.aburaya@hotmail.com>
2022-01-29 11:00:18 -0300

    Führt die Klasse Robot ein.

---
+++ MarsRobot.UnitTests/RobotTests.cs
@@ -0,0 +1,82 @@
+﻿using Xunit;
+
+namespace MarsRobot.UnitTests
+{
+    public class RobotTests
+    {
+        [Theory]
+        [InlineData(0, Robot.CardinalDirection.North)]
+        [InlineData(1, Robot.CardinalDirection.West)]
+        [InlineData(2, Robot.CardinalDirection.South)]
+        [InlineData(3, Robot.CardinalDirection.East)]
+        [InlineData(4, Robot.CardinalDirection.North)]
+        public void TurnLeft_ChangeDirectionCounterClockwise(
+            int repetitions, Robot.CardinalDirection expectedFinalDirection)
+        {
+            var robot = new Robot(1,1);
+            for (int count = 0; count < repetitions; ++count)
+            {
+                robot.TurnLeft();
+            }
+            Assert.Equal(expectedFinalDirection, robot.Direction);
+        }
+
+        [Theory]
+        [InlineData(0, Robot.CardinalDirection.North)]
+        [InlineData(1, Robot.CardinalDirection.East)]
+        [InlineData(2, Robot.CardinalDirection.South)]
+        [InlineData(3, Robot.CardinalDirection.West)]
+        [InlineData(4, Robot.CardinalDirection.North)]
+        public void TurnRight_ChangeDirectionClockwise(
+            int repetitions, Robot.CardinalDirection expectedFinalDirection)
+        {
+            var robot = new Robot(1, 1);
+            for (int count = 0; count < repetitions; ++count)
+            {
+                robot.TurnRight();
+            }
+            Assert.Equal(expectedFinalDirection, robot.Direction);
+        }
+
+        [Fact]
+        public void MoveForward_UntilEndOfPlateuThenTurnRight()
+        {
+            (int x, int y) max = (3,4);
+            var robot = new Robot(max.x, max.y);
+
+            // heading North:
+            for (int y = 1; y <= max.y; ++y)
+            {
+                Assert.Equal((1, y), robot.Position);
+                robot.MoveForward();
+            }
+
+            // heading East:
+            robot.TurnRight();
+            for (int x = 1; x <= max.x; ++x)
+            {
+                Assert.Equal((x, max.y), robot.Position);
+                robot.MoveForward();
+            }
+
+            // heading South:
+            robot.TurnRight();
+            for (int y = max.y; y >= 1; --y)
+            {
+                Assert.Equal((max.x, y), robot.Position);
+                robot.MoveForward();
+            }
+
+            // heading West:
+            robot.TurnRight();
+            for (int x = max.x; x >= 1; --x)
+            {
+                Assert.Equal((x, 1), robot.Position);
+                robot.MoveForward();
+            }
+
+            // back to origin
+            Assert.Equal((1,1), robot.Position);
+        }
+    }
+}
---
+++ MarsRobot/Robot.cs
@@ -0,0 +1,75 @@
+﻿
+namespace MarsRobot
+{
+    /// <summary>
+    /// Implements a robot that receives the commands of <see cref="ICommandReceiver"/>.
+    /// </summary>
+    public class Robot : ICommandReceiver
+    {
+        public enum CardinalDirection { North, West, South, East };
+
+        public CardinalDirection Direction { get; private set; }
+
+        public (int x, int y) Position => _position;
+
+        private (int x, int y) _position;
+
+        private readonly (int x, int y) _max;
+
+        /// <summary>
+        /// Creates the plateau where the robot moves.
+        /// It is a 2-dimensional space with origin (1,1) that extends
+        /// until (<paramref name="maxPlateauX"/>, <paramref name="maxPlateauY"/>).
+        /// </summary>
+        /// <param name="maxPlateauX">Maximum coordinate (inclusive) for the East-West axis.</param>
+        /// <param name="maxPlateauY">Maximum coordinate (inclusive) for the North-South axis.</param>
+        public Robot(int maxPlateauX, int maxPlateauY)
+        {
+            _max.x = maxPlateauX;
+            _max.y = maxPlateauY;
+            _position = (1, 1);
+            Direction = CardinalDirection.North;
+        }
+
+        public void TurnLeft()
+        {
+            int directionIndex = (int)Direction;
+            if (++directionIndex <= (int)CardinalDirection.East)
+                Direction = (CardinalDirection)directionIndex;
+            else
+                Direction = CardinalDirection.North;
+        }
+
+        public void TurnRight()
+        {
+            int directionIndex = (int)Direction;
+            if (--directionIndex >= (int)CardinalDirection.North)
+                Direction = (CardinalDirection)directionIndex;
+            else
+                Direction = CardinalDirection.East;
+        }
+
+        public void MoveForward()
+        {
+            switch (Direction)
+            {
+                case CardinalDirection.North:
+                    if (_position.y < _max.y)
+                        ++_position.y;
+                    break;
+                case CardinalDirection.West:
+                    if (_position.x > 1)
+                        --_position.x;
+                    break;
+                case CardinalDirection.South:
+                    if (_position.y > 1)
+                        --_position.y;
+                    break;
+                case CardinalDirection.East:
+                    if (_position.x < _max.x)
+                        ++_position.x;
+                    break;
+            }
+        }
+    }
+}
